31aaccd066f250d745fa7f0ecabfb4af6653f3cf
Stipanovich_Anthony <Stipanovich_Anthony@student.mahoningctc.com>
2021-02-10 09:16:25 -0500

    first commit

---
+++ Lab/script.js
@@ -0,0 +1,22 @@
+let pet = {
+  type: "Dog-",
+  name: "Larry",
+  numLegs: 4,
+  sayName: function () {
+    return "The name of this dog is " + this.name + ".";
+  },
+  sayLegs: function () {
+    return "This dog has " + this.numLegs + " legs.";
+  },
+};
+
+console.log(pet.sayName());
+console.log(pet.sayLegs());
+
+document.getElementById("dog").innerHTML = pet.type;
+
+document.getElementById("name").innerHTML =
+  "The dogs name is " + pet.name + ".";
+
+document.getElementById("numLegs").innerHTML =
+  "the dog has " + pet.numLegs + " legs.";
